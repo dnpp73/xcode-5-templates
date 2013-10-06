83e03060db0f0e03350914f1667c0dd0673c4d45
Yusuke SUGAMIYA <yusuke.dnpp@gmail.com>
2013-10-06 19:03:54 +0900

    プロジェクト生成時に付く余計なコメントを除去

--- File Templates/Cocoa/Objective-C category.xctemplate/NSObject/___FILEBASENAME___.m
+++ File Templates/Cocoa/Objective-C category.xctemplate/NSObject/___FILEBASENAME___.m
@@ -1,9 +1,5 @@
 #import "___FILEBASENAME___.h"
 
-@interface ___FILEBASENAMEASIDENTIFIER___ ()
-
-@end
-
 @implementation ___VARIABLE_categoryClass:identifier___ (___VARIABLE_categoryName:identifier___)
 
 @end
--- Project Templates/Base/Objective-C Unit Test Bundle.xctemplate/___PACKAGENAMEASIDENTIFIER___.m
+++ Project Templates/Base/Objective-C Unit Test Bundle.xctemplate/___PACKAGENAMEASIDENTIFIER___.m
@@ -1,11 +1,3 @@
-//
-//  ___FILENAME___
-//  ___PACKAGENAME___
-//
-//  Created by ___FULLUSERNAME___ on ___DATE___.
-//___COPYRIGHT___
-//
-
 #import <___UNIT_TEST_FRAMEWORK_NAME___/___UNIT_TEST_FRAMEWORK_NAME___.h>
 
 @interface ___FILEBASENAMEASIDENTIFIER___ : ___UNIT_TEST_BASE_CLASS___
