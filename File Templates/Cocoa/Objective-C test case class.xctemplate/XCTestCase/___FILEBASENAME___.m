fdc665ae218a883284b9886cd4837586540b0cea
Yusuke SUGAMIYA <yusuke.dnpp@gmail.com>
2013-10-10 18:18:05 +0900

    TestCase のテンプレを編集してサブクラス作っても良い感じになるようにした

--- File Templates/Cocoa/Objective-C test case class.xctemplate/XCTestCase/___FILEBASENAME___.m
+++ File Templates/Cocoa/Objective-C test case class.xctemplate/XCTestCase/___FILEBASENAME___.m
@@ -1,4 +1,4 @@
-#import <XCTest/XCTest.h>
+___IMPORTHEADER_testSubclass___
 
 @interface ___FILEBASENAMEASIDENTIFIER___ : ___VARIABLE_testSubclass___
 
@@ -9,18 +9,11 @@
 - (void)setUp
 {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
 - (void)tearDown
 {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
-}
-
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
 }
 
 @end
--- Project Templates/Base/Objective-C Unit Test Bundle.xctemplate/___PACKAGENAMEASIDENTIFIER___.m
+++ Project Templates/Base/Objective-C Unit Test Bundle.xctemplate/___PACKAGENAMEASIDENTIFIER___.m
@@ -9,18 +9,11 @@
 - (void)setUp
 {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
 - (void)tearDown
 {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
-}
-
-- (void)testExample
-{
-    ___UNIT_TEST_SYMBOL_PREFIX___Fail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
 }
 
 @end
